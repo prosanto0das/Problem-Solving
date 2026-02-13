ace8839df1feb3247855ec579ea371a05c1426cc
Prosanto Das <prosanto0das@gmail.com>
2026-02-13 10:38:29 +0600

    Add Container With Most Water solution

---
+++ medium/Container With Most Water.cpp
@@ -0,0 +1,22 @@
+
+//https://leetcode.com/problems/container-with-most-water
+
+// two pointer approch
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+
+        int ans = 0;
+        int left = 0;
+        int right = height.size() - 1;
+        while(left < right)
+        {
+            ans = max(ans, (right - left) * min(height[right], height[left]));
+            if(height[left] <= height[right])left++;
+            else right--;
+        }
+        return ans;
+        
+    }
+};
